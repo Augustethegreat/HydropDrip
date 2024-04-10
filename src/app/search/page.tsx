a9b233327b1a73ce6e0bb158eebfd0e9d685964d
Augustethegreat <102854341+Augustethegreat@users.noreply.github.com>
2024-04-10 15:40:36 +0200

    pushonemoretime

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,23 +1,10 @@
 
-import { fetchResults } from '@/lib/fecthResults';
-import { notFound } from 'next/navigation';
+
 import React from 'react'
 
-type Props = {
-    searchParams: SearchParams;
-  };
-  export type SearchParams = {
-    url: URL;
-    group_adults: string;
-    group_children: string;
-    no_rooms: string;
-    checkin: string;
-    checkout: string;
-  };
-async function SearchPage({searchParams}: Props) {
-    if(!searchParams.url) return notFound();
-    const results = await fetchResults(searchParams);
-    console.log(results);
+
+async function SearchPage() {
+  
   return (
     <div>SearchPage</div>
   )
