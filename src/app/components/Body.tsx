8a2d18c4dea063fcbf2198c4ab8bc22dd11e674e
Augustethegreat <102854341+Augustethegreat@users.noreply.github.com>
2024-04-10 16:10:16 +0200

    pushthis

--- src/app/components/Body.tsx
+++ src/app/components/Body.tsx
@@ -4,13 +4,13 @@ import pic1 from "./img/pic1.png"
 import Image from 'next/image'
 function TheBody() {
   return (
-    <div  className=' w-1200px h-1200px  '>
+    <div  className='w-1200px h-1200px'>
         <div className=' w-120vh lg:w-1200px lg:h-900px mt-7 px-5 lg:px-0'>
           <p className='text-[#14a8d7] text-xl lg:text-4xl font-bold'>Bonjour,</p>
         
           <p className='text-black mt-2  text-sm lg:text-2xl font-bold'>Decouvrez le compte Diasporia, votre compte bancaire maintenu de l'etranger...</p>
        
-            <Image src={pic1} className='w-600 h-600 rounded-lg mt-5' width={920} height={600} alt=""  />
+            <Image src={pic1} className='w-600 h-600 rounded-lg mt-5' width={920} height={600} alt=''  />
          
         </div>
         </div>
