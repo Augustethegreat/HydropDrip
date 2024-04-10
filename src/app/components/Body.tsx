63ca6673585f90fe07c788ff7507314f34aa0782
Augustethegreat <102854341+Augustethegreat@users.noreply.github.com>
2024-04-10 15:52:06 +0200

    pushnow

---
+++ src/app/components/Body.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react'
+import pic1 from "./img/pic1.png"
+import Image from 'next/image'
+function TheBody() {
+  return (
+    <div  className=' w-1200px h-1200px  '>
+        <div className=' w-120vh lg:w-1200px lg:h-900px mt-7 px-5 lg:px-0'>
+          <p className='text-[#14a8d7] text-xl lg:text-4xl font-bold'>Bonjour,</p>
+        
+          <p className='text-black mt-2  text-sm lg:text-2xl font-bold'>Decouvrez le compte Diasporia, votre compte bancaire maintenu de l'etranger...</p>
+          <div className='mt-5'>
+            <Image src={pic1} className='w-600 h-600 rounded-lg' width={920} height={600} alt="" srcset="" />
+          </div>
+        </div>
+        </div>
+  )
+}
+
+export default TheBody
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-"use client"
 import Image from "next/image";
+import TheBody from "./components/Body";
 
 export default function Home() {
   return (
-    <main className="bg-[#013B94]">
-      <section className="max-w-7xl mx-auto p-6">
-        <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
-        <h3 className="text-white py-5 text-xl">
-          Search low prices on hotels, homes and much more...
-        </h3>
-      </section>
-     
-   
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white">
+      
+      <TheBody/>
+    
     </main>
   );
 }
