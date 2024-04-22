b4e4c450df4ecdd346435c338c758eb9982383d8
Augustethegreat <102854341+Augustethegreat@users.noreply.github.com>
2024-04-22 20:22:18 +0200

    nfi

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div  className='text-center items-center justify-center mx-auto'>
-       <div className='mt-[100px] pb-[300px]'>
+       <div className='mt-[200px] pb-[450px]'>
        <p className='text-[40px] text-[#959595] font-bold '>#About Diaspora</p>
        </div>
     </div>
