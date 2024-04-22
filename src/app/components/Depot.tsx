699d25f2a4134f88b899c8d5a039a0823649ed5e
Augustethegreat <102854341+Augustethegreat@users.noreply.github.com>
2024-04-22 11:42:06 +0200

    mypushbumm

--- src/app/components/Depot.tsx
+++ src/app/components/Depot.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function () {
+export default function Depot() {
   return (
-    <div className='text-center items-center justify-center mx-auto'>
+    <div  className='text-center items-center justify-center mx-auto'>
        <div className='mt-[100px] pb-[250px]'>
        <p className='text-[40px] text-[#959595] font-bold '>#Dépôt a termes</p>
        </div>
