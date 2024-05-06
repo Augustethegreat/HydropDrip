bb9590d5b74e98039caff51b9e1dd70c8068bced
Augustethegreat <102854341+Augustethegreat@users.noreply.github.com>
2024-05-06 21:22:58 +0200

    lastpush

--- src/app/twopgage.tsx
+++ src/app/twopgage.tsx
@@ -1,8 +1,8 @@
 "use client";
 import Link from "next/link";
 import { Fragment, useState } from "react";
-import { Bars3Icon, XMarkIcon, EnvelopeIcon, ChatBubbleBottomCenterTextIcon,QuestionMarkCircleIcon, UserCircleIcon, CreditCardIcon, BanknotesIcon, WalletIcon, RectangleGroupIcon } from "@heroicons/react/24/outline";
-import {ChatBubbleLeftIcon,ChevronDownIcon,ChevronUpIcon,HomeIcon,PaperAirplaneIcon,PhoneIcon,PlayCircleIcon} from "@heroicons/react/20/solid";
+import { Bars3Icon, XMarkIcon, EnvelopeIcon, QuestionMarkCircleIcon, UserCircleIcon, CreditCardIcon, BanknotesIcon,   } from "@heroicons/react/24/outline";
+import {ChatBubbleLeftIcon,ChevronDownIcon,ChevronUpIcon,ChatBubbleBottomCenterTextIcon,HomeIcon,PaperAirplaneIcon,PhoneIcon,PlayCircleIcon, RectangleGroupIcon, PhoneXMarkIcon, WalletIcon} from "@heroicons/react/20/solid";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import { SocialIcon } from 'react-social-icons'
 import { cn } from "./lib/utils";
@@ -164,7 +164,7 @@ export default function Home2() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -287,13 +287,13 @@ export default function Home2() {
             <a href="#" className="-m-1.5 p-1.5 text-white">
             <img
               className="w-20 h-auto"
-              src="https://equitygroupholdings.com/cd/Content/assets/img/equity-bank-logo.png"
+              src={logoPic.src}
               alt=""
             />
             </a>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-white"
+              className="-m-2.5 rounded-md p-2.5 text-black"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -304,42 +304,20 @@ export default function Home2() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6 ">
-                <div className="flex gap-x-3">
-                <div>
-                <UserCircleIcon className="h-10 w-10 text-[#a22a2b] mt-4 " aria-hidden="true"/>
-                </div>
-              <div className="py-2 hover:cursor-pointer" onClick={() => setLoginOpen(true)}>
-                <p className="text-white font-medium text-[15px]">Bonjour !</p>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-0 text-base font-medium text-[15px] leading-7 text-[#a22a2b] "
-                >
-                  Login / SignUp
-                </a>
-              </div>
-                </div>
+        
                 <a
                   href="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-white"
-                ><div className="flex gap-x-6 hover:text-white">
-                   <WalletIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
-                   <p className="text-[#272828] font-medium text-[15px]">Accueil</p>
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]">
+                   <HomeIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]">Home</p>
                 </div>
                
                 </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-white"
-                >
-                  <div className="flex gap-x-6 hover:text-white" onClick={visualroute}>
-                   <CreditCardIcon className="h-7 w-7 text-[#616f8d] font-medium text-[15px] mt-0 hover:text-white" aria-hidden="true"/>
-                   <p className="text-[#272828] font-medium text-[15px]"> Crédits</p>
-                </div>
-               
-                </a>
-                <Popover.Group className="flex lg:gap-x-12">
+
+                {/* <Popover.Group className="flex lg:gap-x-12">
           <Popover className="relative">
-            <Popover.Button onClick={change} className="flex gap-x-6 hover:text-white">
+            <Popover.Button onClick={change} className="flex gap-x-6 hover:text-[#45aad7]">
             <BanknotesIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
             <p className="text-[#272828] font-medium text-[15px]">Simulateurs</p>
               <ChevronDownIcon
@@ -380,12 +358,12 @@ export default function Home2() {
               </Popover.Panel>
             </Transition>
           </Popover>
-          </Popover.Group>
+          </Popover.Group> */}
                 {/* 
                 <a
                   href="#depot"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-white"
-                ><div className="flex gap-x-6 hover:text-white" onClick={depotroute}>
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]" onClick={depotroute}>
                    <BanknotesIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
                    <p className="text-[#272828] font-medium text-[15px]">Dépôt a termes</p>
                 </div>
@@ -393,20 +371,39 @@ export default function Home2() {
                 </a> */}
                 <a
                  href="#offres"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-white"
-                ><div className="flex gap-x-6 hover:text-white"  onClick={packroute}>
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]"  onClick={packroute}>
                    <RectangleGroupIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
-                   <p className="text-[#272828] font-medium text-[15px]"> Nos offres</p>
+                   <p className="text-[#272828] font-medium text-[15px]"> Our Solutions</p>
+                </div>
+               
+                </a>
+                <a
+                 href="#offres"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]"  onClick={packroute}>
+                   <WalletIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]"> Services</p>
                 </div>
                
                 </a>
                 <a
                 onClick={aboutroute}
                  href="#about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-white"
-                ><div onClick={aboutroute} className="flex gap-x-6 hover:text-white"  >
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div onClick={aboutroute} className="flex gap-x-6 hover:text-[#45aad7]"  >
+                   <PhoneIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]"> Get In Touch</p>
+                </div>
+               
+                </a>
+                <a
+                onClick={aboutroute}
+                 href="#about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div onClick={aboutroute} className="flex gap-x-6 hover:text-[#45aad7]"  >
                    <ChatBubbleBottomCenterTextIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
-                   <p className="text-[#272828] font-medium text-[15px]"> À propos</p>
+                   <p className="text-[#272828] font-medium text-[15px]"> About Us</p>
                 </div>
                
                 </a>
